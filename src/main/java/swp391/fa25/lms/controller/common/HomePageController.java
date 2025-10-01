fe31b3e5064bf818ce2e8175f71223300e9a5ab2
HuyenVu <huyenvtthe172681@fpt.edu.vn>
2025-10-02 02:38:44 +0700

    FE screen sign-up

--- src/main/java/swp391/fa25/lms/controller/common/HomePageController.java
+++ src/main/java/swp391/fa25/lms/controller/common/HomePageController.java
@@ -1,4 +1,11 @@
 package swp391.fa25.lms.controller.common;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HomePageController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
 }
