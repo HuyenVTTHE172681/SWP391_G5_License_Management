5674ec53201caf1d5a5827707fa53b5f4cd48e73
HuyenVu <huyenvtthe172681@fpt.edu.vn>
2025-10-02 13:47:25 +0700

    test url

--- src/main/java/swp391/fa25/lms/controller/common/HomePageController.java
+++ src/main/java/swp391/fa25/lms/controller/common/HomePageController.java
@@ -1,11 +1,13 @@
 package swp391.fa25.lms.controller.common;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomePageController {
 
     @GetMapping("/")
     public String homePage() {
-        return "home";
+        return "home"; // sẽ trả về file templates/home.html
     }
 }
