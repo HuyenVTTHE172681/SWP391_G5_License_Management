e3c61cccfe6879130d32bc43d247a4abb1015822
HuyenVu <huyenvtthe172681@fpt.edu.vn>
2025-10-07 00:14:56 +0700

    home page

--- src/main/java/swp391/fa25/lms/controller/common/HomePageController.java
+++ src/main/java/swp391/fa25/lms/controller/common/HomePageController.java
@@ -1,13 +1,33 @@
 package swp391.fa25.lms.controller.common;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import swp391.fa25.lms.model.Account;
 
 @Controller
 public class HomePageController {
 
     @GetMapping("/")
-    public String homePage() {
-        return "home"; // sẽ trả về file templates/home.html
+    public String defaultRedirect() {
+        // Default => redirect tới /home
+        return "redirect:/home";
+    }
+
+    // Home
+    @GetMapping("/home")
+    public String showHomePage(HttpServletRequest request, Model model) {
+        Account account = (Account) request.getSession().getAttribute("loggedInAccount");
+        if (account == null) return "redirect:/login";
+        model.addAttribute("account", account);
+        model.addAttribute("maskedPassword", request.getSession().getAttribute("maskedPassword"));
+        return "public/home";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:/login";
     }
 }
