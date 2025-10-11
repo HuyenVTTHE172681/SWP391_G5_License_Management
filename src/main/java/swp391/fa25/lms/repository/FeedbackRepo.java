47bbcf72e25422f69c5de6400c00c195b2ef6cfa
HuyenVu <huyenvtthe172681@fpt.edu.vn>
2025-10-12 01:14:58 +0700

    merge Hoang branch

--- src/main/java/swp391/fa25/lms/repository/FeedbackRepo.java
+++ src/main/java/swp391/fa25/lms/repository/FeedbackRepo.java
@@ -1,17 +1,16 @@
 package swp391.fa25.lms.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import swp391.fa25.lms.model.Feedback;
 import swp391.fa25.lms.model.Tool;
 
-@Repository
-public interface FeedbackRepo extends JpaRepository<Feedback, Long> {
-    // Tinh AVG rating
+public interface FeedBackRepo extends JpaRepository<Feedback, Long> {
+    Page<Feedback> findByTool(Tool tool, Pageable pageable);
+    long countByTool(Tool tool);
     @Query("SELECT AVG(f.rating) FROM Feedback f WHERE f.tool.toolId = :toolId")
     Double findAverageRatingByTool(@Param("toolId") Long toolId);
-
-    Long countByTool(Tool tool);
 }
