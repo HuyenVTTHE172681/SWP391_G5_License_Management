5a5227c43e30df3f1fac9fe89a6cf171c67d3081
Nguyen Huy Hoang <nguyenhuyhoang31102005@gmail.com>
2025-10-12 01:21:14 +0700

    merge HuyenVtT

--- src/main/java/swp391/fa25/lms/repository/FeedBackRepo.java
+++ src/main/java/swp391/fa25/lms/repository/FeedBackRepo.java
@@ -5,12 +5,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import swp391.fa25.lms.model.Feedback;
 import swp391.fa25.lms.model.Tool;
 
+@Repository
 public interface FeedBackRepo extends JpaRepository<Feedback, Long> {
-    Page<Feedback> findByTool(Tool tool, Pageable pageable);
-    long countByTool(Tool tool);
+    // Tinh AVG rating
     @Query("SELECT AVG(f.rating) FROM Feedback f WHERE f.tool.toolId = :toolId")
     Double findAverageRatingByTool(@Param("toolId") Long toolId);
+    Long countByTool(Tool tool);
+    Page<Feedback> findByTool(Tool tool, Pageable pageable);
 }
