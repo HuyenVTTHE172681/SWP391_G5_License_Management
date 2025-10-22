6a4aa80966b9457b15cb95168187b0148dcbdbd4
HuyenVu <huyenvtthe172681@fpt.edu.vn>
2025-10-22 21:06:13 +0700

    Add screen view order history

--- src/main/java/swp391/fa25/lms/repository/CustomerOrderRepository.java
+++ src/main/java/swp391/fa25/lms/repository/CustomerOrderRepository.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 import swp391.fa25.lms.model.CustomerOrder;
 import swp391.fa25.lms.model.Feedback;
 
+import java.util.List;
+
 @Repository
-public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Integer> {
+public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
+    // Lấy danh sách order của 1 user, mới nhất trước
+    List<CustomerOrder> findByAccount_AccountIdOrderByCreatedAtDesc(Long accountId);
 }
