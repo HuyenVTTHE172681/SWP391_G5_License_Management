86b73bba01dfbd84c7904a601afcb6ea7363d309
PhucBH <phucbuihuy456@gmail.com>
2025-11-14 16:33:07 +0700

    Create account

--- src/main/java/swp391/fa25/lms/controller/admin/DashBoardAdminController.java
+++ src/main/java/swp391/fa25/lms/controller/admin/DashBoardAdminController.java
@@ -350,9 +350,9 @@ public class DashBoardAdminController {
         }
 
         // Validate email
-        if (!email.matches("^[A-Za-z0-9._%+-]+@gmail\\.com$")) {
-            model.addAttribute("error", "Email không hợp lệ! Vui lòng nhập đúng định dạng Gmail.");
-            return "admin/account-create";
+        if(!email.contains("@gmail.com")){
+            model.addAttribute("error", "Vui lòng nhập lại Email");
+            return "public/registerGuestToSeller";
         }
 
         // Validate phone
